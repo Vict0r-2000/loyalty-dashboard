58300380de16170c3962a49597e56dd96980a4f8
Victor Cotton <cottonvicto0@gmail.com>
2026-03-19 11:43:40 +0100

    url api railway

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://loyalty-api-production-2f87.up.railway.app/api',
 })
 
 api.interceptors.request.use((config) => {
